aaa5bb1bc42ef5931d71854df363243a5ce8b9d6
troywith77 <ruitang307@gmail.com>
2019-10-22 22:30:00 +0800

    update runtime config

--- src/conf/index.js
+++ src/conf/index.js
@@ -1,9 +1,1 @@
-let conf
-
-if (process.env.VUE_APP_ENV === 'sit') {
-  conf = require('./sit')
-} else {
-  conf = require('./production')
-}
-
-module.exports = conf
+module.exports = require(`./${process.env.VUE_APP_ENV}`)
